ab319c6799a779694b910461558f148e9b1f7ef5
Victor_99 <vamonarrez21@gmail.com>
2025-09-18 14:20:01 -0600

    test5

---
+++ src/assets/topvar.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-topvar',
+  templateUrl: './topvar.component.html',
+  styleUrls: ['./topvar.component.scss'],
+    standalone: true,
+  imports: [CommonModule,  IonicModule,RouterModule] // <-- solo módulos
+
+})
+export class TopvarComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
